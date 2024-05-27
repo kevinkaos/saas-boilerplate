7d954be30473be8ebeeaa77a3021fa21ae9cd264
kev <kev@roddeiMac.local>
2024-05-27 17:33:54 +0800

    init working

---
+++ app/src/client/app/TestAppPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TestAppPage = () => {
+  return (
+    <div className='py-10 lg:mt-10'>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div>TestApp</div>
+      </div>
+    </div>
+  )
+}
+
+export default TestAppPage;
